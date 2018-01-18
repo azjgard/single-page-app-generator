c2336a6151d251b2d9ed99463442387b46bbc5d5
Jordin Gardner <gard.jordin@gmail.com>
2018-01-17 20:27:45 -0800

    added admin ui and spruced up homepage

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -18,9 +18,20 @@ class Admin extends Component {
   render() {
     return (
       <div>
-        <h1>{templateData.h1}</h1>
-        <p>{templateData.p}</p>
-        <p>{calculated}</p>
+        <div className="CreatePage">
+          <h2>P tag</h2>
+          <input type="text" name="p" />
+          <h2>H1 tag</h2>
+          <input type="text" name="h1" />
+          <button
+            onClick={() => {
+              // do some cool stuff
+              console.log("just sumbitted that ish");
+            }}
+          >
+            Submit
+          </button>
+        </div>
       </div>
     );
   }
